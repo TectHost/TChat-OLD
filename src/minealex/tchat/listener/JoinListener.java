e0730145b3a01de2d0d2525e57b35425d80e1829
Tect.host <tect.host@gmail.com>
2023-10-08 09:34:12 +0200

    Add files via upload

--- src/minealex/tchat/listener/JoinListener.java
+++ src/minealex/tchat/listener/JoinListener.java
@@ -17,7 +17,7 @@ import minealex.tchat.TChat;
 
 public class JoinListener implements Listener {
 
-	private final TChat plugin;
+    private final TChat plugin;
 
     public JoinListener(TChat plugin) {
         this.plugin = plugin;
@@ -26,8 +26,10 @@ public class JoinListener implements Listener {
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         try {
-            String joinMessage = getConfiguredJoinMessage(event.getPlayer().getName()); // Obtener el JoinMessage desde el archivo JSON
-            event.setJoinMessage(joinMessage);
+            String joinMessage = getConfiguredJoinMessage(event.getPlayer().getName());
+            if (joinMessage != null) {
+                event.setJoinMessage(joinMessage);
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -36,15 +38,17 @@ public class JoinListener implements Listener {
     @EventHandler
     public void onPlayerQuit(PlayerQuitEvent event) {
         try {
-            String quitMessage = getConfiguredQuitMessage(event.getPlayer().getName()); // Obtener el QuitMessage desde el archivo JSON
-            event.setQuitMessage(quitMessage);
+            String quitMessage = getConfiguredQuitMessage(event.getPlayer().getName());
+            if (quitMessage != null) {
+                event.setQuitMessage(quitMessage);
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    @SuppressWarnings("deprecation")
-	private String getConfiguredJoinMessage(String playerName) {
+    @SuppressWarnings({ "unchecked", "deprecation" })
+    private String getConfiguredJoinMessage(String playerName) {
         try {
             String filePath = plugin.getDataFolder().getPath() + "/format_config.json";
             JSONParser parser = new JSONParser();
@@ -52,42 +56,52 @@ public class JoinListener implements Listener {
             JSONObject jsonObject = (JSONObject) obj;
 
             JSONObject joins = (JSONObject) jsonObject.get("Joins");
+
+            boolean joinMessagesEnabled = (boolean) joins.getOrDefault("joinMessagesEnabled", true);
+
+            if (!joinMessagesEnabled) {
+                return null; // Retorna null si los mensajes de join están deshabilitados
+            }
 
             if (joins != null && joins.containsKey("JoinMessage")) {
                 String joinMessage = (String) joins.get("JoinMessage");
-                joinMessage = joinMessage.replace("%player%", playerName); // Reemplazar %player% con el nombre del jugador
+                joinMessage = joinMessage.replace("%player%", playerName);
                 joinMessage = PlaceholderAPI.setPlaceholders(Bukkit.getOfflinePlayer(playerName), joinMessage);
                 return ChatColor.translateAlternateColorCodes('&', joinMessage);
-            } else {
-                return "&5TChat &e> &7[&a+&7] &a" + playerName + " &7has entered the server";
             }
         } catch (Exception e) {
             e.printStackTrace();
-            return "&5TChat &e> &7[&a+&7] &a" + playerName + " &7has entered the server";
         }
+
+        return null; // Retorna null si no se pudo obtener el mensaje configurado
     }
 
-    @SuppressWarnings("deprecation")
-	private String getConfiguredQuitMessage(String playerName) {
+    @SuppressWarnings({ "unchecked", "deprecation" })
+    private String getConfiguredQuitMessage(String playerName) {
         try {
             String filePath = plugin.getDataFolder().getPath() + "/format_config.json";
             JSONParser parser = new JSONParser();
             Object obj = parser.parse(new FileReader(filePath));
             JSONObject jsonObject = (JSONObject) obj;
 
-            JSONObject joins = (JSONObject) jsonObject.get("Joins");
+            JSONObject quits = (JSONObject) jsonObject.get("Joins");
 
-            if (joins != null && joins.containsKey("QuitMessage")) {
-                String quitMessage = (String) joins.get("QuitMessage");
-                quitMessage = quitMessage.replace("%player%", playerName); // Reemplazar %player% con el nombre del jugador
+            boolean quitMessagesEnabled = (boolean) quits.getOrDefault("quitMessagesEnabled", true);
+
+            if (!quitMessagesEnabled) {
+                return null; // Retorna null si los mensajes de quit están deshabilitados
+            }
+
+            if (quits != null && quits.containsKey("QuitMessage")) {
+                String quitMessage = (String) quits.get("QuitMessage");
+                quitMessage = quitMessage.replace("%player%", playerName);
                 quitMessage = PlaceholderAPI.setPlaceholders(Bukkit.getOfflinePlayer(playerName), quitMessage);
                 return ChatColor.translateAlternateColorCodes('&', quitMessage);
-            } else {
-                return "&5TChat &e> &7[&c-&7] &c" + playerName + " &7has left the server";
             }
         } catch (Exception e) {
             e.printStackTrace();
-            return "&5TChat &e> &7[&c-&7] &c" + playerName + " &7has left the server";
         }
+
+        return null; // Retorna null si no se pudo obtener el mensaje configurado
     }
 }
